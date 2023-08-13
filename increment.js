e107d7794b53c4eca4cb22732cb910354c1cdab4
SABBIR AHMED <sabbir.react@gmail.com>
2023-08-13 18:57:40 +0600

    v2

---
+++ increment.js
@@ -0,0 +1,5 @@
+// console.log(React);
+// console.log(ReactDOM);
+const domContainer = document.querySelector("#root");
+const myElement = React.createElement("div", null, "Hello World");
+ReactDOM.render(myElement, domContainer);
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let number = 0;
+let number1 = 0;
 const button = document.querySelector("#button");
 const display = document.querySelector("#display");
+const button1 = document.querySelector("#button1");
+const display1 = document.querySelector("#display1");
 button.addEventListener("click", () => {
   number++;
   display.textContent = number;
+});
+button1.addEventListener("click", () => {
+  number1++;
+  display1.textContent = number1;
 });
